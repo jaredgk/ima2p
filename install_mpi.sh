aba64c69ba6ea015d5d8c48d8dfe76c2784fa71a
Jared Knoblauch <jaredknoblauch@gmail.com>
2017-01-24 17:36:06 -0500

    A

--- install_mpi.sh
+++ install_mpi.sh
@@ -1,7 +1,7 @@
 wget --no-check-certificate https://www.open-mpi.org/software/ompi/v2.0/downloads/openmpi-2.0.1.tar.gz
-sudo -E apt-get -yq --no-install-recommends --force-yes install libibnetdisc-dev
-tar -xvzf openmpi-2.0.1.tar.gz
+#sudo apt-get -yq --no-install-recommends --force-yes install libibnetdisc-dev
+tar -xzf openmpi-2.0.1.tar.gz
 cd openmpi-2.0.1
 ./configure
 make
-sudo make install
+sudo make install >/dev/null
